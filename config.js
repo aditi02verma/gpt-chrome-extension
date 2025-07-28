81005b0b91c655b27a98ece34ac6b0809ae263bd
aditi02verma <aditiverma1000s@gmail.com>
2025-07-28 14:36:30 +0530

    Initial commit: ChatGPT Chrome Extension

---
+++ config.js
@@ -0,0 +1,12 @@
+import Default from "./plugins/Default.js";
+import Image from "./plugins/Image.js";
+import Gangster from "./plugins/Gangster.js";
+
+const config = {
+  plugins: [
+    Default,
+    
+  ],
+};
+
+export default config;
